e185064442cceb3eb17743823ac0d4784ebd57cb
Sathish Gnanaprakasam <7434440+statsinr@users.noreply.github.com>
2018-10-08 14:28:13 +0530

    Create postResults.php

---
+++ starter/postResults.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+  <head>
+    <title>GET VALUE</title>
+  </head>
+  <body>
+    <?php
+    
+      //1. get the first value submitted via POST method from postValue.html
+      $first = 
+      
+      //2. get the second value submitted via POST method from postValue.html
+      $second = 
+      
+      //3. compute and print the sum
+      
+    ?>
+  </body>
+</html>
