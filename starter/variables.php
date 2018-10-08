36b4ba0578de83f6590f742868fe3ec502fb5852
Sathish Gnanaprakasam <7434440+statsinr@users.noreply.github.com>
2018-10-08 14:14:42 +0530

    Create variables.php

---
+++ starter/variables.php
@@ -0,0 +1,10 @@
+<?php
+  //1. Create two variables $first , $second and assign values
+  //234 , 456 rspectively
+  
+  
+  //2. Create third variable $result
+  //to store the sum of $first , $second variables
+    
+  //3. Print the $result
+?>
