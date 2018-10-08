ea6e3a153f61dac4c419acbf8913452d147d4fb5
Sathish Gnanaprakasam <7434440+statsinr@users.noreply.github.com>
2018-10-08 14:26:31 +0530

    Create getResults.php

---
+++ starter/getResults.php
@@ -0,0 +1,20 @@
+
+<!doctype html>
+<html>
+  <head>
+    <title>GET VALUE</title>
+  </head>
+  <body>
+    <?php
+    
+      //1. get the first value submitted via GET method from getValue.html
+      $first = 
+      
+      //2. get the second value submitted via GET method from getValue.html
+      $second = 
+      
+      //3. compute and print the sum
+      
+    ?>
+  </body>
+</html>
