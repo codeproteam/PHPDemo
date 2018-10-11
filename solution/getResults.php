bb2d7fb5daf5caf8aef926d064925b70b5e04c2f
Sathish Gnanaprakasam <7434440+statsinr@users.noreply.github.com>
2018-10-11 10:44:06 +0530

    Update getResults.php

--- solution/getResults.php
+++ solution/getResults.php
@@ -6,14 +6,14 @@
   <body>
     <?php
     
-      //1. get the first value submitted via GET method from getValue.html
-      $first = $_GET['first']
+      /*1. get the first value submitted via GET method from getValue.html */
+      $first = $_GET['first'];
       
-      //2. get the second value submitted via GET method from getValue.html
-      $second = $_GET['second']
+      /*2. get the second value submitted via GET method from getValue.html*/
+      $second = $_GET['second'];
       
-      //3. compute and print the sum
-      echo (int)$first + (int)$second
+      /*3. compute and print the sum*/
+      echo (int)$first + (int)$second;
     ?>
   </body>
 </html>
