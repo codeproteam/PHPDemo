967270de13c0278941b91099aea14596967ec53f
Sathish Gnanaprakasam <7434440+statsinr@users.noreply.github.com>
2018-10-11 10:46:48 +0530

    Update postResults.php

--- solution/postResults.php
+++ solution/postResults.php
@@ -1,18 +1,18 @@
 <!doctype html>
 <html>
   <head>
-    <title>GET VALUE</title>
+    <title>POST VALUE</title>
   </head>
   <body>
     <?php
     
-      //1. get the first value submitted via POST method from postValue.html
+      /* 1. get the first value submitted via POST method from postValue.html */
       $first = $_POST['first'];
       
-      //2. get the second value submitted via POST method from postValue.html
+      /* 2. get the second value submitted via POST method from postValue.html */
       $second = $_POST['second'];
       
-      //3. compute and print the sum
+      /* 3. compute and print the sum */
       echo (int)$first + (int)$second;
     ?>
   </body>
